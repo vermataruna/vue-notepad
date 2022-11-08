5d5d62daad7753ec802472544be99dbb5a2d2c18
vermataruna <tarunaverma792@gmail.com>
2022-11-09 00:15:15 +0530

    modify view notes screen

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,31 @@
 <template>
     <div class="notes">
-        <h1>Notes</h1>
+        <div class="card has-background-success-dark p-4 mb-5">
+            <div class="field">
+            <div class="control">
+            <textarea class="textarea" placeholder="Add a new note"/>
+            </div>
+            </div>
+
+            <div class="field is-grouped is-grouped-right">
+            <div class="control">
+            <button class="button is-link has-background-success-">
+                Add new note
+            </button>
+            </div>
+            </div>
+        </div>
+
+        <div class="card mb-4">
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsa, reprehenderit quam ullam dolores facere ea laboriosam corrupti officia molestiae perferendis? Rem ad officiis nulla voluptates asperiores. Adipisci, harum autem.
+                </div>
+            </div>
+        <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+        </div>
     </div>
 </template>
