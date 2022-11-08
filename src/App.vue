aaf8aa790023f988c5c0a23fcaa0c2b339e7c031
vermataruna <tarunaverma792@gmail.com>
2022-11-08 23:56:06 +0530

    add navbar menu and enhance the ui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> |
-  <RouterLink to="/stats">Stats</RouterLink>
-  <RouterView />
+  <Navbar />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+import Navbar from '@/components/layouts/Navbar.vue'
+
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
