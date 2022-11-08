4b7426de36c8d1ffbb52851e7804b8399d249ff2
vermataruna <tarunaverma792@gmail.com>
2022-11-09 00:56:30 +0530

    add create new note option

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,23 +3,30 @@
         <div class="card has-background-success-dark p-4 mb-5">
             <div class="field">
             <div class="control">
-            <textarea class="textarea" placeholder="Add a new note"/>
+            <textarea class="textarea" 
+                      placeholder="Add a new note"
+                      v-model="newNote"
+                      ref="newNoteRef"/>
             </div>
             </div>
 
             <div class="field is-grouped is-grouped-right">
             <div class="control">
-            <button class="button is-link has-background-success-">
+            <button class="button is-link has-background-success"
+                    @click="addNote"
+                    :disabled="!newNote">
                 Add new note
             </button>
             </div>
             </div>
         </div>
 
-        <div class="card mb-4">
+        <div class="card mb-4" 
+             v-for="note in notes" 
+             :key="note.id">
             <div class="card-content">
                 <div class="content">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsa, reprehenderit quam ullam dolores facere ea laboriosam corrupti officia molestiae perferendis? Rem ad officiis nulla voluptates asperiores. Adipisci, harum autem.
+                    {{ note.content }}
                 </div>
             </div>
         <footer class="card-footer">
@@ -29,3 +36,29 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const newNote = ref('')
+const newNoteRef = ref(null)
+
+const notes = ref([
+    {
+        id: 'id1',
+        content: 'My first sample note for testing' 
+    },
+])
+
+const addNote = () => {
+    let currentDate = new Date().getTime(), id = currentDate.toString()
+    let note = {
+        id,
+        content: newNote.value
+    }
+    notes.value.unshift(note)
+    newNote.value = ''
+    newNoteRef.value.focus()
+}
+
+</script>
