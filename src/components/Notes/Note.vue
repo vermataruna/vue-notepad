b7caa964b35bef06b794b52c64daa5a19e63babb
vermataruna <tarunaverma792@gmail.com>
2022-12-04 17:21:12 +0530

    add: seperate note component

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card mb-4">
+        <div class="card-content">
+            <div class="content">
+                    {{ note.content }}
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item">Edit</a>
+            <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    note: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -21,24 +21,18 @@
             </div>
         </div>
 
-        <div class="card mb-4" 
-             v-for="note in notes" 
-             :key="note.id">
-            <div class="card-content">
-                <div class="content">
-                    {{ note.content }}
-                </div>
-            </div>
-        <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-        </footer>
-        </div>
+        <Note 
+            v-for="note in notes" 
+            :key="note.id"
+            :note="note"    
+        />
+
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Note from '@/components/Notes/Note.vue';
 
 const newNote = ref('')
 const newNoteRef = ref(null)
