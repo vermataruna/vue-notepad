c0cd1a6bb76129d3c0ed7bbb9e6cfe0df3cb6d24
vermataruna <tarunaverma792@gmail.com>
2023-01-03 14:43:08 +0530

    add: notes stats table with data

--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,5 +1,30 @@
 <template>
     <div class="stats">
-        <h1>Stats</h1>
+        <table class="table is-fullwidth">
+            <thead>
+              <tr>
+                <th>Stat</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td>Number of notes</td>
+                  <td>{{ storeNotes.getTotalNotesCount }}</td>
+                </tr>
+                <tr>
+                  <td>Number of characters(all notes)</td>
+                  <td>{{ storeNotes.getTotalCharactersCount }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
+
+<script setup>
+import { useStoreNotes } from '@/stores/storeNotes'
+
+const storeNotes = useStoreNotes()
+
+
+</script>
