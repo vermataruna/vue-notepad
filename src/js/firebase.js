94881753f2cd3305bf6999513da06ab4bdad36d6
vermataruna <tarunaverma792@gmail.com>
2023-01-05 23:55:19 +0530

    add: auth store setup

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBlLeeiKM0QE672TH1z4ve27ZM1xiZJRYw",
@@ -14,5 +15,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+const auth = getAuth(app)
 
-export { db }
+export { 
+    db,
+    auth
+}
---
+++ src/stores/storeAuth.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'
+import { auth } from '@/js/firebase'
+
+export const useStoreAuth = defineStore('storeAuth', {
+  state: () => {},
+  actions: {},
+  getters: {}
+})
