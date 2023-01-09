2e228415e0990f04b5c5eb835697ec55fd97ca48
vermataruna <tarunaverma792@gmail.com>
2023-01-10 00:06:41 +0530

    add: router to the store and redirect the user after login and logout actions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//adding plugin to use vue router in pinia store
+//reference link: https://pinia.vuejs.org/core-concepts/plugins.html#adding-new-external-properties:~:text=secret%27%0A%7D)-,Adding%20new%20external%20properties,-%C2%B6
+
+const pinia = createPinia()
+
+pinia.use(({ store }) => {
+    store.router = markRaw(router) //markRaw will make router non-reactive
+  })
+
 createApp(App)
-    .use(createPinia())
+    .use(pinia)
     .use(router)
     .mount('#app')
