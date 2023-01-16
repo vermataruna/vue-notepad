08ce69beacf362c7bb432c38e8e630574fcc921b
vermataruna <tarunaverma792@gmail.com>
2023-01-16 23:58:02 +0530

    add: navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import ViewNotes from '@/views/ViewNotes.vue'
 import EditNote from '@/views/EditNote.vue'
 import ViewStats from '@/views/ViewStats.vue'
 import AuthView from '@/views/AuthView.vue'
+import { useStoreAuth } from '@/stores/storeAuth'
 
 const routes = [
     {
@@ -30,6 +31,17 @@ const routes = [
 const router = createRouter({
     history: createWebHashHistory(),
     routes
+})
+
+//navigation guards
+router.beforeEach(async (to, from) => {
+    const storeAuth = useStoreAuth()
+    if(!storeAuth.user.id && to.name !== 'auth'){
+        return { name: 'auth' }
+    }
+    if(storeAuth.user.id && to.name === 'auth'){
+        return false
+    }    
 })
 
 export default router
